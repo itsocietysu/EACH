14b29c2ac04670764e920740a87ed746d68b0a61
Michail Nakhatovich <mish_1998@mail.ru>
2018-07-09 14:17:41 +0300

    Created 'Login' button
    Sign in and registration buttons, login page with form and empty
    registration page were created.

--- Frontend/app/containers/App/index.js
+++ Frontend/app/containers/App/index.js
@@ -13,6 +13,8 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
 import FeaturePage from 'containers/FeaturePage/Loadable';
+import LoginPage from 'containers/LoginPage/Loadable';
+import RegistrationPage from 'containers/RegistrationPage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
@@ -40,6 +42,8 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/features" component={FeaturePage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/registration" component={RegistrationPage} />
         <Route path="" component={NotFoundPage} />
       </Switch>
       <Footer />
